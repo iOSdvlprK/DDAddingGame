c42db044fcdad7793da7eaec98556a1be5a67c0c
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-02-14 23:01:25 +0900

    Initial Commit

---
+++ DDAddingGame/DDAddingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDAddingGameApp.swift
+//  DDAddingGame
+//
+//  Created by joe on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DDAddingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
