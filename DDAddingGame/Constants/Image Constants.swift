0d249083e3c25d61120fa6105df9c3d371061d82
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-02-21 00:47:10 +0900

    assets: creatures and bubbles

---
+++ DDAddingGame/Constants/Image Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Image Constants.swift
+//  DDAddingGame
+//
+//  Created by joe on 2/21/25.
+//
+
+import Foundation
+
+enum Creatures: String {
+    case creature0, creature1, creature2, creature3, creature4, creature5
+}
